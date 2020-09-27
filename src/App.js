76ec184773fb264333a17caf80585f5b2d9dfc03
Elmehdi <eaitbrahim@gmail.com>
2020-09-27 14:09:03 +0100

    Add router and navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Game from './pages/Game';
+import HighScores from './pages/HighScores';
+import GameOver from './pages/GameOver';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Router>
+      <Navbar>
+        <Switch>
+          <Route path='/game' component={Game} />
+          <Route path='/highScores' component={HighScores} />
+          <Route path='/gameOver' component={GameOver} />
+          <Route path='/' component={Home} />
+        </Switch>
+      </Navbar>
+    </Router>
   );
 }
 
