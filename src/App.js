f55c289ff324864df7bbe51fbff2bfdf857db978
Elmehdi <eaitbrahim@gmail.com>
2020-09-28 00:33:55 +0100

    Navigation and base styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Game from './pages/Game';
@@ -7,18 +6,26 @@ import HighScores from './pages/HighScores';
 import GameOver from './pages/GameOver';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import { Container } from './styled/Conatiner';
+import { Main } from './styled/Main';
+import Global from './styled/Global';
 
 function App() {
   return (
     <Router>
-      <Navbar>
-        <Switch>
-          <Route path='/game' component={Game} />
-          <Route path='/highScores' component={HighScores} />
-          <Route path='/gameOver' component={GameOver} />
-          <Route path='/' component={Home} />
-        </Switch>
-      </Navbar>
+      <Global />
+      <Main>
+        <Container>
+          <Navbar>
+            <Switch>
+              <Route path='/game' component={Game} />
+              <Route path='/highScores' component={HighScores} />
+              <Route path='/gameOver' component={GameOver} />
+              <Route path='/' component={Home} />
+            </Switch>
+          </Navbar>
+        </Container>
+      </Main>
     </Router>
   );
 }
